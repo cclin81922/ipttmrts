5aa5395f9491b772473cecc1f06d5085aa2180bc
cclin <jimlintw922@gmail.com>
2018-10-07 21:12:03 +0800

    mod main.ipToTaipeiMRTStation()

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // Station ...
@@ -14,10 +15,18 @@ type Station struct {
 	NameEN    string
 	Latitude  float64
 	Longitude float64
+	Distance  float64
 }
 
 func (s Station) String() string {
 	return fmt.Sprintf("%s", s.NameTW)
+}
+
+func (s *Station) setDistanceAwayFrom(latitude, longitude float64) {
+
+	// TODO
+	s.Distance = 0
+
 }
 
 // IData ...
@@ -42,98 +51,86 @@ func ipStrToNetIP(ip string) net.IP {
 	return netIP
 }
 
-func merge(distance ...<-chan float64) <-chan float64 {
-	out := make(chan float64)
-
-	// TODO
-
-	return out
-}
-
-func distanceOfTwoPointsByLatitudeLongitude(latitude1, longitude1, latitude2, longitude2 float64) <-chan float64 {
-	out := make(chan float64)
-
-	// TODO
-
-	return out
-}
-
 func ipToLatitudeLongitude(ip net.IP) (float64, float64) {
 	// TODO
 	return 0, 0
 }
 
 func ipToTaipeiMRTStation(ip net.IP) Station {
-	stations := []Station{
-		{"南港展覽館", "", 0, 0},
-		{"昆陽", "", 0, 0},
-		{"後山埤", "", 0, 0},
-		{"永春", "", 0, 0},
-		{"市政府", "", 0, 0},
-		{"國父紀念館", "", 0, 0},
-		{"忠孝敦化", "", 0, 0},
-		{"忠孝復興", "", 0, 0},
-		{"忠孝新生", "", 0, 0},
-		{"善導寺", "", 0, 0},
-		{"台北車站", "", 0, 0},
-		{"西門", "", 0, 0},
-		{"龍山寺", "", 0, 0},
-		{"江子翠", "", 0, 0},
-		{"新埔", "", 0, 0},
-		{"板橋", "", 0, 0},
-		{"府中", "", 0, 0},
-		{"亞東醫院", "", 0, 0},
-		{"海山", "", 0, 0},
-		{"土城", "", 0, 0},
-		{"永寧", "", 0, 0},
-		{"頂埔", "", 0, 0},
-		{"淡水", "", 0, 0},
-		{"紅樹林", "", 0, 0},
-		{"竹圍", "", 0, 0},
-		{"關渡", "", 0, 0},
-		{"忠義", "", 0, 0},
-		{"復興崗", "", 0, 0},
-		{"北投", "", 0, 0},
-		{"新北投", "", 0, 0},
-		{"奇岩", "", 0, 0},
-		{"唭哩岸", "", 0, 0},
-		{"石牌", "", 0, 0},
-		{"明德", "", 0, 0},
-		{"芝山", "", 0, 0},
-		{"士林", "", 0, 0},
-		{"劍潭", "", 0, 0},
-		{"圓山", "", 0, 0},
-		{"民權西路", "", 0, 0},
-		{"雙連", "", 0, 0},
-		{"中山", "", 0, 0},
-		{"台大醫院", "", 0, 0},
-		{"中正紀念堂", "", 0, 0},
-		{"東門", "", 0, 0},
-		{"大安森林公園", "", 0, 0},
-		{"大安", "", 0, 0},
-		{"信義安和", "", 0, 0},
-		{"台北101/世貿", "", 0, 0},
-		{"象山", "", 0, 0},
+	stations := []*Station{
+		&Station{NameTW: "南港展覽館", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "昆陽", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "後山埤", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "永春", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "市政府", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "國父紀念館", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "忠孝敦化", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "忠孝復興", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "忠孝新生", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "善導寺", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "台北車站", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "西門", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "龍山寺", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "江子翠", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "新埔", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "板橋", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "府中", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "亞東醫院", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "海山", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "土城", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "永寧", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "頂埔", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "淡水", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "紅樹林", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "竹圍", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "關渡", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "忠義", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "復興崗", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "北投", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "新北投", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "奇岩", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "唭哩岸", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "石牌", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "明德", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "芝山", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "士林", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "劍潭", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "圓山", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "民權西路", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "雙連", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "中山", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "台大醫院", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "中正紀念堂", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "東門", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "大安森林公園", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "大安", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "信義安和", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "台北101/世貿", Latitude: 0, Longitude: 0},
+		&Station{NameTW: "象山", Latitude: 0, Longitude: 0},
 	}
-	_ = stations
 
 	latitude, longitude := ipToLatitudeLongitude(ip)
 
-	distances := make([]<-chan float64, 0)
+	wg := sync.WaitGroup{}
+	wg.Add(len(stations))
 	for _, station := range stations {
-		distance := distanceOfTwoPointsByLatitudeLongitude(station.Latitude, station.Longitude, latitude, longitude)
-		distances = append(distances, distance)
+		go func(station *Station) {
+			defer wg.Done()
+			station.setDistanceAwayFrom(latitude, longitude)
+		}(station)
 	}
+	wg.Wait()
 
 	var minDistance float64
-	for distance := range merge(distances...) {
-		if minDistance > distance {
-			minDistance = distance
+	var nearStation *Station
+	for _, station := range stations {
+		if minDistance > station.Distance {
+			minDistance = station.Distance
+			nearStation = station
 		}
 	}
 
-	// TODO return the station of minimum distance
-	return Station{NameTW: "西門", NameEN: "Ximen"}
+	return *nearStation
 }
 
 func main() {
