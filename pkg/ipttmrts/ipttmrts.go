2c65cf9253865df4f34d52742fcd53f2dcf2002a
Jim Lin <jimlintw922@gmail.com>
2018-10-09 16:06:09 +0800

    add comment

--- pkg/ipttmrts/ipttmrts.go
+++ pkg/ipttmrts/ipttmrts.go
@@ -12,7 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-// Package ipttmrts ...
+// Package ipttmrts is for mapping your current IP address to the nearest Taipei MRT station.
 package ipttmrts
 
 import (
